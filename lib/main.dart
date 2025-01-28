4a632fd84e22d236029a0abdaf09e8df27697488
Johnny-Simba <yossefmahmuod830@gmail.com>
2025-01-28 15:19:47 +0200

    Add register Screen. Change Home route to RegisterScreenView.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:movies_app/register_screen_view/register_screen_view.dart';
 import 'home.dart';
 import 'utils/BlocObserver.dart';
 import 'utils/app_theme.dart';
@@ -19,11 +19,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-
       darkTheme: AppTheme.darkTheme,
       theme: AppTheme.lightTheme,
       themeMode: ThemeMode.dark,
-      home: const Home(),
-      );
+      home: RegisterScreenView(),
+    );
   }
 }
---
+++ lib/register_screen_view/register_screen_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreenView extends StatelessWidget {
+  static const String routeName = 'register';
+
+  const RegisterScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
