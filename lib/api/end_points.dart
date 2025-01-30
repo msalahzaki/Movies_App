d1210cc7ce26d2b11678b5cb3c35a1c3169a6672
msalahzaki <m.salah.zaki@gmail.com>
2025-01-30 10:43:07 +0200

    Add API Url And End Points

---
+++ lib/api/end_points.dart
@@ -0,0 +1,25 @@
+class EndPoints {
+
+  ///Movies EndPoints
+
+  static const String listMovies = "/v2/list_movies.json";
+  static const String movieDetails = "/v2/movie_details.json";
+  static const String movieSuggestions = "/v2/movie_suggestions.json";
+
+
+  ///Authentication EndPoints
+
+  static const String login = "/auth/login" ;
+  static const String register = "/auth/register" ;
+  static const String resetPassword = "/auth/reset-password" ;
+
+  /// Favorites  EndPoints
+  static const String favoriteAdd = "/favorites/add" ;
+  static const String favoritesAll = "/favorites/all" ;
+  static const String favoritesRemoveMovieId = "/favorites/remove/movieId" ;
+  static const String isFavorite = "/favorites/is-favorite/movieId" ;
+
+  /// Profile
+  static const String profile = "/profile" ;
+
+}
